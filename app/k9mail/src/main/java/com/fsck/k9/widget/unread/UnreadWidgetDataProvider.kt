f8ef47feee288eef85425a662e5e0aebc3cda1fa
Hannes Achleitner <hannes.software@gmx.at>
2021-07-01 16:22:05 +0200

    Rebranding

--- app/k9mail/src/main/java/com/fsck/k9/widget/unread/UnreadWidgetDataProvider.kt
+++ app/k9mail/src/main/java/com/fsck/k9/widget/unread/UnreadWidgetDataProvider.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.content.Intent
 import com.fsck.k9.Account
 import com.fsck.k9.Preferences
-import com.fsck.k9.R
+import com.fsck.l10.R
 import com.fsck.k9.activity.MessageList
 import com.fsck.k9.controller.MessagingController
 import com.fsck.k9.mailstore.FolderRepositoryManager
